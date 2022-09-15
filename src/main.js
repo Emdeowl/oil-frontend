d8446ca3d1a58c3df5c8655508620cb997cba0ad
JaeHyuk <kwon850550@naver.com>
2022-09-15 18:09:26 +0900

    App.vue에 Nav연결

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from "vue";
+import { createRouter, createWebHashHistory } from "vue-router";
+import { createApolloProvider } from "@vue/apollo-option";
+import apolloClient from "./vue-apollo";
 
-createApp(App).mount('#app')
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+});
+
+const routes = [{ path: "/" }];
+
+import App from "./App.vue";
+const router = createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: createWebHashHistory(),
+    routes, // short for `routes: routes`
+  });
+
+const app = createApp({
+  render: () => h(App),
+});
+
+app.use(router);
+app.use(apolloProvider);
+app.mount("#app");
