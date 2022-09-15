c40c9817336a80e0d58d79b847fd7950fde700ba
JaeHyuk <kwon850550@naver.com>
2022-09-15 18:15:45 +0900

    Articles.vue 생성

---
+++ src/containers/Articles.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+      <div class="section">
+        <div class="container">
+          <h1>Strapi blog</h1>
+  
+          <ArticlesList :articles="articles"></ArticlesList>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import ArticlesList from "../components/ArticlesList.vue";
+  import gql from "graphql-tag";
+  
+  export default {
+      data() {
+          return {
+              articles: [],
+            };
+        },
+        components: {
+          ArticlesList,
+        },
+    apollo: {
+      articles: gql`
+        query Articles {
+          articles {
+            data {
+              attributes {
+                slug
+                title
+                category {
+                  data {
+                    attributes {
+                      slug
+                      name
+                    }
+                  }
+                }
+                image {
+                  data {
+                    attributes {
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+    },
+  };
+  </script>
+  
+<style>
+
+</style>
